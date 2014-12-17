d92bd25c8c8b6a6bf7c533572949d7c1e76f42df
James Morse <james.f.morse@gmail.com>
2014-12-16 22:49:33 -0600

    James' implementation of stubbed assignment functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+###################################################################
+# makeCacheMetrix()
+#
+# Input Parameters: A square, invertible matrix
+# Output Return: A list containing functions/methods
+#           to get and set the matrix's value and/or its inverse
+###################################################################
 makeCacheMatrix <- function(x = matrix()) {
-
+     ## initialize inverse matrix variable to NULL
+     invX <- NULL
+     
+     ## define get/set functions
+     set <- function(setVal = matrix()) {
+          x <-- setVal
+          # empty inverse matrix variable when resetting matrix value
+          invX <-- NULL
+     }
+     get <- function() x
+     setInverse <- function(inverse) invX <-- inverse
+     getInverse <- function() invX
+     
+     ## return special matrix object as list of 4 function pointers
+     list(
+          set = set,
+          get = get,
+          setInverse = setInverse,
+          getInverse = getInverse
+     )
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+########################################################################
+# cacheSolve()
+#
+# Input Parameters: A "special" square, invertible
+#                     matrix list object
+# Output Return:    The inverted matrix value of the
+#                     input matrix value
+# Algorithm:       If list object already contains inverse matrix value,
+#                     then that cached value is returned.
+#                   If not, solve() is executed on the raw matrix value
+#########################################################################
+cacheSolve <- function(specialMatrix, ...) {
+  #retrieve inverse value and check whether set (not NULL)
+  inverse <- specialMatrix$getInverse()
+  if (!is.null(inverse)) {
+      message("**retrieving inverse of matrix via cache**")
+      return(inverse)
+  }
+  
+  #if code reaches this point, we must compute inverted matrix at runtime
+  rawMatrix <- specialMatrix$get()
+  inverse <- solve(rawMatrix)
+  specialMatrix$setInverse(inverse)
+  ## return computed inverse of special matrix object 
+  inverse
 }
